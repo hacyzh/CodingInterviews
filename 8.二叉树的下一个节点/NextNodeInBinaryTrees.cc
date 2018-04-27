192be6d8cf4a7713a944e95f26ba1cd8468405a2
hacyzh <hacyzh.163.com>
2018-04-27 23:35:04 +0800

    07_ConstructBinaryTree

---
+++ 8.二叉树的下一个节点/NextNodeInBinaryTrees.cc
@@ -0,0 +1,27 @@
+ ///
+ /// @file    NextNodeInBinaryTrees.cc
+ /// @author  haiy(hacyzh@163.com)
+ /// @date    2018-04-27 23:18:47
+ ///
+ 
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct BinaryTreeNode
+{
+	int				m_nValut;
+	BinaryTreeNode* m_pLeft;
+	BinaryTreeNode*	m_pRight;
+	BinaryTreeNode* m_Parent;
+};
+
+BinaryTreeNode *GetNext(BianryTreeNode* pNode)
+{
+
+}
+int main()
+{
+ 
+}
